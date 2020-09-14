a80d50b39e44a02e95a9e6bafd1103211f5beb01
Alexander Hashkov <dev.alx.hashey@gmail.com>
2020-09-14 23:27:26 +0300

    Let`s Make This FuN ^_^

---
+++ script.js
@@ -0,0 +1,64 @@
+// const invoice = 100;
+// const stock = 100;
+
+// // Write code under this line
+// let message;
+
+// if (invoice <= stock) {
+//   console.log((message = "Заказ оформлен, с вами свяжется менеджер"));
+// } else {
+//   console.log((message = "На складе недостаточно товаров!"));
+// }
+///////////////////////////////////////////////////////////
+
+// const userPassword = "jqueryismyjam";
+
+// const ADMIN_PASSWORD = "jqueryismyjam";
+
+// const CANCELED_BY_USER = "Отменено пользователем!";
+// const ACCESS_IS_ALLOWED = "Добро пожаловать!";
+// const ACCESS_DENIED = "Доступ запрещен, неверный пароль!";
+// let message;
+
+// const passwordField = prompt("Please enter password:");
+// console.log(passwordField);
+
+// if (passwordField === null) {
+//   console.log((message = CANCELED_BY_USER));
+// } else {
+//   if ((passwordField === userPassword, passwordField === ADMIN_PASSWORD)) {
+//     console.log((message = ACCESS_IS_ALLOWED));
+//   } else {
+//     console.log((message = ACCESS_DENIED));
+//   }
+// }
+
+///////////////////////////////////////////////////////////
+
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// const CANCELED_BY_USER = "Отменено пользователем!";
+// const ACCESS_DENIED = "Недостаточно средств на счету!";
+
+// let quantityField = prompt("Please enter quantity:");
+// console.log(quantityField);
+
+// let orderPieces = quantityField;
+
+// let totalPrice = pricePerDroid * orderPieces; // Write code on this line
+// let balanceCredit = credits - totalPrice; // Write code on this line
+// let message;
+
+// if (quantityField === null) {
+//   console.log((message = CANCELED_BY_USER));
+// } else {
+//   if (totalPrice > credits) {
+//     console.log((message = ACCESS_DENIED));
+//   } else {
+//     console.log(
+//       (message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`)
+//     );
+//   }
+// }
+
+///////////////////////////////////////////////////////////
