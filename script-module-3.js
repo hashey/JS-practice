a62de91cc85898b6899b272039fbf98c6a2298d0
Alexander Hashkov <dev.alx.hashey@gmail.com>
2020-10-16 21:01:53 +0300

    module3 - done

--- script-module-3.js
+++ script-module-3.js
@@ -86,7 +86,7 @@ console.log(countProps({a:1, b:1, c:1, d:1, e:1})); // 5
 Поиск наибольшего значения среди свойств объекта
 Напиши функцию findBestEmployee(employees), которая принимает объект сотрудников и возвращает имя самого продуктивного(который выполнил больше всех задач).Сотрудники и кол - во выполненных задач содержатся как свойства объекта в формате "имя": "кол-во задач".
 */
-
+/*
 const findBestEmployee = function (employees) {
   'use strict';
   // Write code under this line
@@ -133,3 +133,223 @@ const sellers = {
 }
 console.log(findBestEmployee(sellers)); 
 // 'lux'  
+*/
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+// Задача 3-4
+// Суммирование значений свойств объекта
+// Напиши функцию countTotalSalary(employees) принимающую объект зарплат.
+// Функция считает общую сумму зарплаты работников и возвращает ее.
+// Каждое поле объекта, передаваемого в функцию, имеет вид "имя": "зарплата".
+/*
+const countTotalSalary = function (employees) {
+  'use strict';
+  // Write code under this line
+  let result = 0;
+  const values = Object.values(employees);
+  for (const value of values) {
+    result += value;
+  }
+
+  return result;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+console.log(countTotalSalary(developers));
+// 1000
+
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+console.log(countTotalSalary(supports));
+// 500
+*/
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+// Задача 3-5
+// Оператор in и метод push
+// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства.
+
+// Функция возвращает массив значений определенного свойства prop из каждого объекта в массиве.
+
+// Используй метод push для добавления значения в массив и оператор in для проверки наличия свойства в объекте.
+/*
+function getAllPropValues(array, prop) {
+  'use strict';
+  // Write code under this line
+  const result = [];
+  for (const product of array) {
+    if (prop in product) {
+      result.push(product[prop]);
+    }
+  }
+
+  return result;
+}
+
+// Объекты и ожидаемый результат
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Радар', price: 1280, quantity: 2 },
+  { name: 'Радар', price: 1320, quantity: 1 },
+  { name: 'Сканер', price: 2700, quantity: 1 },
+  { name: 'Сканер', price: 2500, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+console.log(getAllPropValues(products, 'name'));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity'));
+// [4, 2, 1, 1, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category'));
+//  []
+*/
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+/*
+Задача 3-6
+Суммирование значений свойства из массива объектов
+Напиши функцию calculateTotalPrice(allProdcuts, productName), которая получает массив объектов и имя продукта(значение свойства name).
+Возвращает общую стоимость продукта(цена умноженная на количество).
+
+Вызовы функции для проверки работоспособности твоей реализации.
+
+// calculateTotalPrice(products, 'Радар'));
+// 9080
+
+// calculateTotalPrice(products, 'Сканер')); // 10200
+
+// calculateTotalPrice(products, 'Захват')); // 2400
+
+// calculateTotalPrice(products, 'Дроид')); // 2800
+*/
+/*
+function calculateTotalPrice(array, prop) {
+  'use strict';
+  // Write code under this line
+
+  let result = 0;
+
+  for (const product of array) {
+    // console.log(product.name);
+    if (prop === product.name) {
+      result += product.price * product.quantity;
+
+      // console.log(result);
+    }
+  }
+
+  return result;
+}
+
+// Объекты и ожидаемый результат
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Радар', price: 1280, quantity: 2 },
+  { name: 'Радар', price: 1320, quantity: 1 },
+  { name: 'Сканер', price: 2700, quantity: 1 },
+  { name: 'Сканер', price: 2500, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+console.log(calculateTotalPrice(products, 'Радар'));
+// 9080
+
+console.log(calculateTotalPrice(products, 'Сканер'));
+// 10200
+
+console.log(calculateTotalPrice(products, 'Захват'));
+// 2400
+
+console.log(calculateTotalPrice(products, 'Дроид'));
+// 2800
+*/
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+// Задание 7 - дополнительное, выполнять не обязательно
+// Напиши скрипт управления личным кабинетом интернет банка.
+// Есть объект account в котором необходимо реализовать методы для работы с балансом и историей транзакций.
+
+/*
+ * Типов транзацкий всего два.
+ * Можно положить либо снять деньги со счета.
+ */
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+/*
+ * Каждая транзакция это объект со свойствами: id, type и amount
+ */
+
+const account = {
+  // Текущий баланс счета
+  balance: 0,
+
+  // История транзакций
+  transactions: [],
+
+  /*
+   * Метод создает и возвращает объект транзакции.
+   * Принимает сумму и тип транзакции.
+   */
+  createTransaction(amount, type) {},
+
+  /*
+   * Метод отвечающий за добавление суммы к балансу.
+   * Принимает сумму транзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций
+   */
+  deposit(amount) {},
+
+  /*
+   * Метод отвечающий за снятие суммы с баланса.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций.
+   *
+   * Если amount больше чем текущий баланс, выводи сообщение
+   * о том, что снятие такой суммы не возможно, недостаточно средств.
+   */
+  withdraw(amount) {},
+
+  /*
+   * Метод возвращает текущий баланс
+   */
+  getBalance() {},
+
+  /*
+   * Метод ищет и возвращает объект транзации по id
+   */
+  getTransactionDetails(id) {},
+
+  /*
+   * Метод возвращает количество средств
+   * определенного типа транзакции из всей истории транзакций
+   */
+  getTransactionTotal(type) {},
+};
