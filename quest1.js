8463b11b6bc605893071c4ccf712381d439e3e10
Alexander Hashkov <dev.alx.hashey@gmail.com>
2020-11-07 00:37:53 +0200

    gsgsd

--- quest1.js
+++ quest1.js
@@ -1,37 +1,37 @@
-function list(names) {
-  //your code here
-  let array = [];
-  let result = '';
+// function list(names) {
+//   //your code here
+//   let array = [];
+//   let result = '';
 
-  for (const item of names) {
-    array.push(item.name);
-  }
-  if (array.length < 2) {
-    result = array.join('');
-  } else if (array.length === 2) {
-    array.splice(array.length - 1, 0, ' & ');
+//   for (const item of names) {
+//     array.push(item.name);
+//   }
+//   if (array.length < 2) {
+//     result = array.join('');
+//   } else if (array.length === 2) {
+//     array.splice(array.length - 1, 0, ' & ');
 
-    result = array.join('');
-  } else if (array.length >= 3) {
-    const lastTwo = array.slice(array.length - 2);
+//     result = array.join('');
+//   } else if (array.length >= 3) {
+//     const lastTwo = array.slice(array.length - 2);
 
-    array.splice(array.length - 2, 2);
+//     array.splice(array.length - 2, 2);
 
-    lastTwo.splice(lastTwo.length - 1, 0, ' & ');
+//     lastTwo.splice(lastTwo.length - 1, 0, ' & ');
 
-    result = array.join(', ') + ', ' + lastTwo.join('');
-  }
+//     result = array.join(', ') + ', ' + lastTwo.join('');
+//   }
 
-  // result = array.join(', ');
+//   // result = array.join(', ');
 
-  return console.log(result);
-}
+//   return console.log(result);
+// }
 
-list([
-  { name: 'Bart' },
-  { name: 'Lisa' },
-  { name: 'Maggie' },
-  { name: 'Homer' },
-  { name: 'Marge' },
-]);
-// returns 'Bart, Lisa & Maggie'
+// list([
+//   { name: 'Bart' },
+//   { name: 'Lisa' },
+//   { name: 'Maggie' },
+//   { name: 'Homer' },
+//   { name: 'Marge' },
+// ]);
+// // returns 'Bart, Lisa & Maggie'
